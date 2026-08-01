7d359586388cf9326f62d9953a24a55150328ab7
maxjurreau-3 <maxjurreau@gmail.com>
2026-08-01 18:28:04 -0400

    Initialize React app in index.tsx

---
+++ portal-os-v4/index.tsx
@@ -0,0 +1,11 @@
+// portal-os-v4/index.tsx
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app';
+import { startPortalOS } from './kernel/bootstrap';
+
+startPortalOS();
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App />);
