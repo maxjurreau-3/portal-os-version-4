aed56485b4af09808f4291013ff7ea9f845e7395
maxjurreau-3 <maxjurreau@gmail.com>
2026-08-01 19:52:25 -0400

    Add Jest configuration for testing

---
+++ portal-os-v4/tests/jest.config.js
@@ -0,0 +1,8 @@
+// portal-os-v4/tests/jest.config.js
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+};
