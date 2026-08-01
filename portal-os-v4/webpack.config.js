eee03e04dc400822c221eb0001d9a245ac1e7521
maxjurreau-3 <maxjurreau@gmail.com>
2026-08-01 19:38:21 -0400

    Refactor webpack config for output path and loaders
    Updated output path resolution and added CSS loader rules.

--- portal-os-v4/webpack.config.js
+++ portal-os-v4/webpack.config.js
@@ -1,15 +1,25 @@
 // portal-os-v4/webpack.config.js
 
+const path = require('path');
+
 module.exports = {
   entry: './index.tsx',
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/dist',
+    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
-    rules: [{ test: /\.tsx?$/, use: 'ts-loader' }],
+    rules: [
+      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
   },
 };
