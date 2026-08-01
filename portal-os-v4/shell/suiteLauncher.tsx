29ef81f24a8aa7dbafa7af483896065c70c83709
maxjurreau-3 <maxjurreau@gmail.com>
2026-08-01 18:23:17 -0400

    Add SuiteLauncher component for suite selection

---
+++ portal-os-v4/shell/suiteLauncher.tsx
@@ -0,0 +1,17 @@
+// portal-os-v4/shell/suiteLauncher.tsx
+
+import React from 'react';
+
+export default function SuiteLauncher() {
+  return (
+    <div>
+      <h2>Suite Launcher</h2>
+      <ul>
+        <li>Sports Suite</li>
+        <li>Quantum Suite</li>
+        <li>XR Suite</li>
+        <li>Identity Suite</li>
+      </ul>
+    </div>
+  );
+}
