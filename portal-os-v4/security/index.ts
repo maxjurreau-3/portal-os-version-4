8ca9d393e255a431c734986fd9df7e3e2a1481b3
maxjurreau-3 <maxjurreau@gmail.com>
2026-08-01 20:29:41 -0400

    Create index.ts to export security modules

---
+++ portal-os-v4/security/index.ts
@@ -0,0 +1,8 @@
+// portal-os-v4/security/index.ts
+
+export * from './auth';
+export * from './guard';
+export * from './integrity';
+export * from './suiteAccess';
+export * from './signalValidation';
+export * from './kernelGuard';
